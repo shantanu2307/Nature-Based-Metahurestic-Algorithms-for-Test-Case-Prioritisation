a7583efc26e398d0d52f41d8f945d984806524c5
Shantanu Goyal <shantanugoyal23@gmail.com>
2022-11-07 19:44:49 +0530

    feat: add fitness summary

--- difference_of_fitness.py
+++ difference_of_fitness.py
@@ -11,17 +11,21 @@ df['Max'] = df[[col for col in df.columns if 'Trial' in col]].max(axis=1)
 df['Min'] = df[[col for col in df.columns if 'Trial' in col]].min(axis=1)
 df['Avg'] = df[[col for col in df.columns if 'Trial' in col]].mean(axis=1)
 df['Count'] = df[[col for col in df.columns if 'Trial' in col]].ge(0).sum(axis=1)
-
-# Print N, IT, Max and Min
-print(df[['N', 'IT', 'Max', 'Min', 'Avg', 'Count']])
+df['Median']=df[[col for col in df.columns if 'Trial' in col]].median(axis=1)
 
 # Plot a 3d surface plot between N, IT and Count
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-surf=ax.plot_trisurf(df['N'], df['IT'], df['Count'], cmap=plt.cm.coolwarm, linewidth=0.2)
+surf=ax.plot_trisurf(df['N'], df['IT'], df['Median'], cmap=plt.cm.coolwarm, linewidth=0.2)
 ax.set_xlabel('N')
 ax.set_ylabel('IT')
-ax.set_zlabel('Count')
+ax.set_zlabel('Median')
 plt.colorbar(surf, shrink=0.5, aspect=5)
-ax.scatter(df['N'], df['IT'], df['Count'], c='black', marker='o')
-plt.show()
+ax.scatter(df['N'], df['IT'], df['Median'], c='black', marker='o')
+
+
+
+df=df[['N', 'IT', 'Max', 'Min', 'Avg', 'Count', 'Median']]
+
+# Export min, max, avg, count, median, n, it to another excel
+df.to_csv('csv/fitness_summary.csv', index=False)
