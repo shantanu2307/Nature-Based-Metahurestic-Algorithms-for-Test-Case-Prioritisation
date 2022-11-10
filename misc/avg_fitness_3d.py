b438c980c202c3b1573407e449d39c63129a2691
Shantanu Goyal <shantanugoyal23@gmail.com>
2022-11-10 10:24:29 +0530

    refactor: folder structure

--- misc/avg_fitness_3d.py
+++ misc/avg_fitness_3d.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('csv/ak.csv')
+df = pd.read_csv('csv/cab_booking.csv')
 
 # Make a coloumn that has all the values set to 61
 # df['Max Fitness'] = 61
